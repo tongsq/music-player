94c430f3cfb91d20bc590b42669816d4374726c4
tongsq <tongsq77@qq.com>
2017-12-12 20:18:40 +0800

    增加鼠标移入显示播放进度时间

--- src/components/progress.js
+++ src/components/progress.js
@@ -31,24 +31,46 @@ export class PlayProgress extends Component{
         isActive: false
     }
     constructor(props){
-        super(props);
-        this.changeProgress = this.changeProgress.bind(this);
+        super(props)
+        this.state = {showTime: false}
+        this.changeProgress = this.changeProgress.bind(this)
+        this.showPlayTime = this.showPlayTime.bind(this)
+        this.hidePlayTime = this.hidePlayTime.bind(this)
     }
     
     changeProgress(e){
-        let progressBar = this.refs.progressBar;
-        let progress = (e.clientX - progressBar.getBoundingClientRect().left) / progressBar.clientWidth;      
-        this.props.onProgressChange(progress.toFixed(4));
+        let progressBar = this.refs.progressBar
+        let progress = (e.clientX - progressBar.getBoundingClientRect().left) / progressBar.clientWidth
+        this.props.onProgressChange(progress.toFixed(4))
+    }
+    showPlayTime(){
+        this.setState({showTime:true})
+    }
+    hidePlayTime(){
+        this.setState({showTime:false})
     }
     render() {
-
+        let {progress} = this.props
+        let timeBoxStyle = {opacity: 0}
+        if (progress < 50){
+            timeBoxStyle.left = progress + '%'
+        }else{
+            timeBoxStyle.right = 100-progress + '%'
+        }
+        if (this.state.showTime){
+            timeBoxStyle.opacity = 1
+        }
         return (
-            <div className="components-playprogress-body" ref="progressBar" onClick={this.changeProgress}>
+            <div className="components-playprogress-body" ref="progressBar" 
+                 onClick={this.changeProgress} 
+                 onMouseOver={this.showPlayTime} 
+                 onMouseOut={this.hidePlayTime}>
                 <div className="components-playprogress">
                     <div className="progress" style={{width:`${this.props.progress}%`}}>
                         <i className={this.props.isActive? "active":""}></i>
                     </div>
                 </div>
+                <div className="time-box" style={timeBoxStyle}>00:58/04:00</div>
             </div>
         );
     }
